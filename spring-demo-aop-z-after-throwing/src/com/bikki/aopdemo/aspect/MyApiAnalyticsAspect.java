268fc64b90b5e3cffbba8b0c4a2207a0aeb532fc
bikkimahato <bikkimahato123@gmail.com>
2020-08-08 16:36:42 +0530

    AOP: AfterThrowing

---
+++ spring-demo-aop-z-after-throwing/src/com/bikki/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -0,0 +1,17 @@
+package com.bikki.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(3)
+public class MyApiAnalyticsAspect {
+
+	@Before("com.bikki.aopdemo.aspect.BikkiAOPExpressions.forDAOPackagewithoutGetterAndSetter()")
+	public void performApiAnalytics() {
+		System.out.println("\n====> API Analytics");
+	}
+}
